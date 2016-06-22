3a1c9bc7cd1ee482c14a4ce7b11aeeb34863dda6
JackDavies <28daviesja@gmail.com>
2016-06-22 23:54:34 +0100

    Create About.cs

---
+++ src/About.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Msharp {
+    class About {
+        private static double  versionNumber = 0.1;
+
+        public static double getVersionNumber() {
+            return versionNumber;
+        }
+
+    }
+}
